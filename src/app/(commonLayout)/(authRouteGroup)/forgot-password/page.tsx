563e44476b3624d67202efd062ddc0b83036ef8c
joykbiswas <joykumarbiswas100@gmail.com>
2026-03-28 00:17:37 +0600

    Project init setup

---
+++ src/app/(commonLayout)/(authRouteGroup)/forgot-password/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ForgotPasswordPage = () => {
+    return (
+        <div>
+            <h3>ForgotPassWord ..</h3>
+        </div>
+    );
+};
+
+export default ForgotPasswordPage;
---
+++ src/app/(commonLayout)/(authRouteGroup)/verify-email/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VerifyEmailPage = () => {
+    return (
+        <div>
+            VerifyEmailPage
+        </div>
+    );
+};
+
+export default VerifyEmailPage;
